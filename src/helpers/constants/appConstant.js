41020508a2890db38957a90ae27d76afd9a28140
Parth6232 <guptaparth089@gmail.com>
2026-05-30 17:09:29 +0530

    Update apiBaseURL to production endpoint

--- src/helpers/constants/appConstant.js
+++ src/helpers/constants/appConstant.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const appConstants = {
-  apiBaseURL: ' http://localhost:3030/api/',
+  // apiBaseURL: ' http://localhost:3030/api/',
+  apiBaseURL: 'https://patientpoint-backend.onrender.com',
   localStorage: {
     tokenKey: 'token',
     refreshTokenKey:'refreshToken',
